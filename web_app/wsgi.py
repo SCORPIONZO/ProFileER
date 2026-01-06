05d9c0617832be638d5ca142d5284b4b87ea1a77
kirillatabaev <scorpionzo@mail.ru>
2026-01-06 22:09:34 +0300

    Change directory

--- web_app/wsgi.py
+++ web_app/wsgi.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Добавляем папку web_app в путь Python
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from app import create_app
 
 # Создаем приложение для Vercel
@@ -5,3 +11,6 @@ app = create_app()
 
 # Vercel ожидает объект wsgi_app
 wsgi_app = app
+
+# Определяем application как точку входа для WSGI
+application = wsgi_app
